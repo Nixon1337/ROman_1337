beb698358b72f9ef5e4b26a777fe22a94f0e0a20
Nixon1337 <99614014+Nixon1337@users.noreply.github.com>
2023-07-19 20:19:53 +0300

    Delete androidTest/java/com/example/flyingaround directory

--- androidTest/java/com/example/flyingaround/pages/Page.kt
+++
@@ -1,7 +0,0 @@
-package com.example.flyingaround.pages
-
-abstract class Page<T>{
-    inline operator fun <R> invoke(block: T.() -> R): R {
-        return block.invoke(this as T)
-    }
-}
--- androidTest/java/com/example/flyingaround/utils/PassengersData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.flyingaround.utils
-
-object PassengersData {
-
-    const val ONE = "1"
-    const val TWO = "2"
-    const val THREE = "3"
-}
